c8b8943d1cf15581b02f3024d5f279c0c07c39d2
Suraj Mavuleti <surajmavuleti@gmail.com>
2026-09-06 20:19:10 +0530

    Feat(v8): Visual Overhaul using CustomTkinter Modern UI Framework

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# ARCH LINUX NATIVE AUTO-UPDATER
 cd /home/suraj/.gemini/antigravity/scratch/ultimate_suite/zero-dsp-arch-linux
 git pull origin main --quiet
-python3 zero_dsp_gui.py
+/home/suraj/.gemini/antigravity/scratch/v8_env/bin/python3 zero_dsp_gui.py
